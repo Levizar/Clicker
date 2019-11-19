33434bcd81581a55c3a295cd7616e4fe425750a5
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2019-11-19 23:09:17 +0100

    first design with model cookies

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,15 @@
 
 (() => {
    
-
-    document.getElementById("clicker").addEventListener('click', ()=>{ 
+    let cible = document.getElementById("cookiesNumber")
+    document.getElementById("clicker").addEventListener('click', (e)=>{ 
        alert(" + 1 cookie !!!!")
+       console.log(event.type)
+       const testi = cible.innerHTML = e.currentTarget.id
+
+       console.log(testi)
        
-      
+      console.log(String.fromCharCode(74,101,32, 116, 39,97,105,109,101,32,71,97,98,121), ":D")
 
     });
 
