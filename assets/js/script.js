58a90582cef942f81f49070426ee214fb31d2c4c
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-11-19 19:16:37 +0100

    script.js - comment A LOT

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,25 +13,50 @@ let arrTypeOfProducer = [];
 class Producer {
     // Permet d'initialiser un proucer avec un nom, une production de base et un prix de base
     constructor(name, baseProduction, baseprice) {
-        this.name = name; // Nom de ce type de producteur
-        this.baseProduction = baseProduction; // Cookie par seconde de base que produit le producer
-        this.nbr = 0; // Le nombre de producer de ce type
-        this.multiplicateur = 1; // Le multiplicateur actuel de ce producer
-        this.totalProductionPerSec = 0; // Production totale de ce type de producer
-        this.baseprice = baseprice; // Le prix de base de ce producer
-        this.price = basePrice; // Le prix actuel de ce producer
+        // Nom de ce type de producteur
+        this.name = name; 
+        // Cookie par seconde de base que produit le producer
+        this.baseProduction = baseProduction; 
+        // Le nombre de producer de ce type
+        this.nbr = 0; 
+        // Le multiplicateur actuel de ce producer
+        this.multiplicateur = 1; 
+        // Production totale de ce type de producer
+        this.totalProductionPerSec = 0; 
+        // Le prix de base de ce producer
+        this.baseprice = baseprice; 
+        // Le prix actuel de ce producer
+        this.price = basePrice; 
+        // Methode recalculant la valeur de totalProductionPerSec
+        this.recalculationProdPerSec = () => {
+            this.totalProductionPerSec = this.baseProduction * this.multiplicateur * this.nbr;
+        }
+        
         // Methode permettant d'acheter un producer et d'augmenter son prix
         // Et d'appeler la fonction recalculant la variable cookie seconde
-        this.buy = () => { 
-            this.nbr += 1;
-            this.totalProductionPerSec = this.baseProduction * this.multiplicateur * this.nbr;
-            this.price = this.basePrice * Math.pow(1, this.nbr); // Il faut remplacer 1 par le logarithme neperien
-            getCookiePerSec();
+        this.buy = () => {
+            // ne lance le contenu de la methode que s'il y a de quoi payer
+            // Au lieu de faire ça, il serait aussi possible de prévoir de désactiver le 
+            // bouton d'achat, voir de faire les 2 pour être sûr. (et de le griser au passage)
+            if ( cookie >= this.price ){
+                // Paiement du prix en cookie
+                cookie -= this.price;
+                // Achat donc augmentation du nbr de producer de ce type
+                this.nbr += 1;
+                // Augmentation du prix du prochain producer
+                this.price = this.basePrice * Math.pow(2, this.nbr);   // Il faut remplacer 1 par le logarithme neperien
+                // Appel la méthode recalculant la propriété totalProductionPerSec de ce producer
+                this.recalculationProdPerSec    ////////!!!!\\\\\\\  Je ne sais pas s'il faut les () ou pas.
+                // lance la fonction recalculant la variable globale cookiePerSec
+                getCookiePerSec();   
+            }
         }
+        // Une fois le type de producteur instancié, stock l'instance        
         arrTypeOfProducer.push(this);
     }
 }
 
+// Fonction globale permettant de calculer le nombre de cookie par second pour le setInterval
 const getCookiePerSec = () => {
     cookiePerSec = 0;
     arrTypeOfProducer.forEach(producer => {
